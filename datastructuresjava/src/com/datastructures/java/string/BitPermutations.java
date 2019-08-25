e17ceb5e5ab5de68313bb8792063f448f484cdfa
jeevanrpro <jeevanrpro@gmail.com>
2019-08-25 23:52:37 +0530

    BitPerm

---
+++ datastructuresjava/src/com/datastructures/java/string/BitPermutations.java
@@ -0,0 +1,78 @@
+package com.datastructures.java.string;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class BitPermutations {
+
+	static long sumBit = 0;
+
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("/workspaces/java/inBitPermutations.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		for (int i = 0; i < t; i++) {
+			String row_col = br.readLine();
+			String row_binary = Integer.toBinaryString(Integer.parseInt(row_col));
+			System.out.println(permuteBit(row_binary));
+			sumBit = 0;
+		}
+	}
+
+	private static String permuteBit(String bitValue) {
+		/*
+		 * char[] bitCharValues = bitValue.toCharArray();
+		 * Arrays.parallelSort(bitCharValues); Set<String> bitPermuteList = new
+		 * HashSet<>(); bitPermuteList.add(String.valueOf(bitCharValues));
+		 * permute(String.valueOf(bitCharValues), 0, bitCharValues.length - 1,
+		 * bitPermuteList); int sumBit = 0; for (String longValue : bitPermuteList)
+		 * sumBit += Integer.parseInt(longValue, 2); System.out.println(sumBit);
+		 * sumBit=0;
+		 */
+		char[] bitCharValues_ = bitValue.toCharArray();
+		Arrays.parallelSort(bitCharValues_);
+		permute(String.valueOf(bitCharValues_), 0, bitCharValues_.length - 1);
+		return String.valueOf(sumBit);
+	}
+
+	private static void permute(String str, int l, int r, final Set<String> bitPermuteList) {
+		if (l == r)
+			bitPermuteList.add(str);
+		else {
+			for (int i = l; i <= r; i++) {
+				str = swap(str, l, i);
+				bitPermuteList.add(str);
+				permute(str, l + 1, r, bitPermuteList);
+				str = swap(str, l, i);
+			}
+		}
+	}
+
+	private static void permute(String str, int l, int r) {
+		if (l == r) {
+
+			sumBit += Integer.parseInt(str, 2);
+
+		} else {
+			for (int i = l; i <= r; i++) {
+				str = swap(str, l, i);
+				permute(str, l + 1, r);
+				str = swap(str, l, i);
+			}
+		}
+	}
+
+	public static String swap(String a, int i, int j) {
+		char temp;
+		char[] charArray = a.toCharArray();
+		temp = charArray[i];
+		charArray[i] = charArray[j];
+		charArray[j] = temp;
+		return String.valueOf(charArray);
+	}
+
+}
