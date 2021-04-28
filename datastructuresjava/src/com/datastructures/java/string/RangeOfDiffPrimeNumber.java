6fbc61649b4850f5a8002087abd38425c8a81113
jeevanrpro <jeevanrpro@gmail.com>
2021-04-28 18:00:01 +0530

    test2

---
+++ datastructuresjava/src/com/datastructures/java/string/RangeOfDiffPrimeNumber.java
@@ -0,0 +1,23 @@
+package com.datastructures.java.string;
+
+public class RangeOfDiffPrimeNumber {
+
+    public static void main(String args[] ) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String size=Integer.parseInt(reader.readLine());
+
+        5
+        5 5
+        2 7
+        8 10
+        10 20
+        4 5
+
+        0
+        5
+                -1
+        8
+        0
+    }
+
+}
