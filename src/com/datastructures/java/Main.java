76cd4951283c3bc83aa259f8de52c6b76789f22a
Jeevan Rangaramaiah <jeevan.rangaramaiah@lntinfotech.com>
2022-12-18 23:02:53 +0530

    updated with docs

--- src/com/datastructures/java/Main.java
+++ src/com/datastructures/java/Main.java
@@ -2,6 +2,7 @@ package com.datastructures.java;
 
 public class Main {
 
+    //maintaining streak
     public static void main(String[] args) {
         USer uSer = null;
         System.out.println(uSer.getAge());
